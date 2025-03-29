42d87abe3a0f0df1bd40eb79b632e25fa327f968
K.Kimura <dotnsf@gmail.com>
2025-03-29 18:05:46 +0900

    deepl enabled

--- app.js
+++ app.js
@@ -69,15 +69,13 @@ app.post( '/api/translate', function( req, res ){
 });
 
 app.post( '/api/pre_wxa', function( req, res ){
-  res.contentType( 'application/json; charset=utf8' );
-  var msg = req.body;
-  if( typeof msg == 'string' ){ msg = JSON.parse( msg ); }
-  //if( typeof msg.payload == 'string' ){ msg.payload = JSON.parse( msg.payload ); }
-  if( msg && msg.deepl ){
-    var source_lang = msg.deepl.source_lang;
-    var target_lang = msg.deepl.target_lang;
-    var text = msg.payload;
-    if( text && source_lang && target_lang ){
+  res.contentType( 'text/plain; charset=utf8' );
+  var text = req.body.text;
+  
+  if( text ){
+    var source_lang = req.query.source_lang;
+    var target_lang = req.query.target_lang;
+    if( source_lang && target_lang ){
       var params = new URLSearchParams();
       params.append( 'text', text );
       params.append( 'target_lang', target_lang );
@@ -89,58 +87,46 @@ app.post( '/api/pre_wxa', function( req, res ){
       if( authkey ){
         axios.post( '/v2/translate', params )
         .then( function( result ){
-          //console.log( {result} );  //. result.data.translations = [ { detected_source_language: "JA", text: "Hello" }, .. ]
-          //. 元の req.body オブジェクトのフォーマットのまま翻訳結果だけを入れ替えて返す
           if( result && result.data && result.data.translations && result.data.translations.length > 0 && result.data.translations[0].text ){
-            msg.payload = result.data.translations[0].text;
+            text = result.data.translations[0].text;
           }
-          res.write( JSON.stringify( msg, null, 2 ) );
+          res.write( text );   //. これが msg.payload に入る
           res.end();
         }).catch( function( err ){
-          msg.warning = err;
-          res.write( JSON.stringify( msg, null, 2 ) );
+          console.log( {err} );
+          res.write( text );
           res.end();
         });
       }else{
-        msg.warning = 'no AUTHKEY specified as env variable.';
-        res.write( JSON.stringify( msg, null, 2 ) );
+        console.log( 'no AUTHKEY specified as env variable.' );
+        res.write( text );
         res.end();
       }
     }else{
-      msg.warning = 'no text and/or target_lang specified in request body.';
-      res.write( JSON.stringify( msg, null, 2 ) );
+      console.log( 'no source_lang and/or target_lang specified.' );
+      res.write( text );
       res.end();
     }
   }else{
-    //. 翻訳せずそのまま出力
-    msg.warning = 'no deepl information found in request body.';
-    res.write( JSON.stringify( msg, null, 2 ) );
+    console.log( 'no text specified in payload.' );
+    res.write( '' );
     res.end();
   }
 });
 
 app.post( '/api/post_wxa', async function( req, res ){
   res.contentType( 'application/json; charset=utf8' );
-  var msg = req.body;
-  if( typeof msg == 'string' ){ msg = JSON.parse( msg ); }
-  if( typeof msg.payload == 'string' ){ msg.payload = JSON.parse( msg.payload ); }
-  msg.num = 0;
-  if( msg && msg.deepl ){
-  msg.num = 1;
-    var source_lang = msg.deepl.source_lang;
-    var target_lang = msg.deepl.target_lang;
-    var text = ( msg.payload && msg.payload.output && msg.payload.output.generic && msg.payload.output.generic.length > 0 && msg.payload.output.generic[0].text ) ? msg.payload.output.generic[0].text : '';
+  var payload = req.body;
+  if( typeof payload == 'string' ){ payload = JSON.parse( payload ); }
+  if( payload ){
+    var source_lang = req.query.source_lang;
+    var target_lang = req.query.target_lang;
+    var text = ( payload && payload.output && payload.output.generic && payload.output.generic.length > 0 && payload.output.generic[0].text ) ? payload.output.generic[0].text : '';
     if( text && source_lang && target_lang ){
-  msg.num = 2;
-  msg.length = msg.payload.output.generic.length;
       if( authkey ){
-        for( var i = 0; i < msg.payload.output.generic.length; i ++ ){
-  msg.num = 3;
-  msg.i = i;
-          text = msg.payload.output.generic[i].text;
+        for( var i = 0; i < payload.output.generic.length; i ++ ){
+          text = payload.output.generic[i].text;
           if( text ){
-  msg.num = 4;
-  msg.text = text;
             var params = new URLSearchParams();
             params.append( 'text', text );
             params.append( 'target_lang', target_lang );
@@ -150,35 +136,30 @@ app.post( '/api/post_wxa', async function( req, res ){
             }
         
             var result = await axios.post( '/v2/translate', params );
-  msg.num = 5;
             if( typeof result == 'string' ){ result = JSON.parse( result ); }
 
-            //console.log( {result} );  //. result.data.translations = [ { detected_source_language: "JA", text: "Hello" }, .. ]
             //. 元の req.body オブジェクトのフォーマットのまま翻訳結果だけを入れ替えて返す
             if( result && result.data && result.data.translations && result.data.translations.length > 0 && result.data.translations[0].text ){
-  msg.num = 6;
-  msg.translations_text = result.data.translations[0].text;
-              msg.payload.output.generic[i].text = result.data.translations[0].text;
+              payload.output.generic[i].text = result.data.translations[0].text;
             }
           }
         }
 
-        res.write( JSON.stringify( msg, null, 2 ) );
+        res.write( JSON.stringify( payload, null, 2 ) );
         res.end();
       }else{
-        msg.warning = 'no AUTHKEY specified as env variable.';
-        res.write( JSON.stringify( msg, null, 2 ) );
+        payload.warning = 'no AUTHKEY specified as env variable.';
+        res.write( JSON.stringify( payload, null, 2 ) );
         res.end();
       }
     }else{
-      msg.warning = 'no text and/or target_lang specified in request body.';
-      res.write( JSON.stringify( msg, null, 2 ) );
+      payload.warning = 'no text and/or target_lang specified in request body.';
+      res.write( JSON.stringify( payload, null, 2 ) );
       res.end();
     }
   }else{
-    //. 翻訳せずそのまま出力
-    msg.warning = 'no deepl information found in request body.';
-    res.write( JSON.stringify( msg, null, 2 ) );
+    payload.warning = 'no deepl information found in request body.';
+    res.write( JSON.stringify( payload, null, 2 ) );
     res.end();
   }
 });
